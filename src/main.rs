ba6caf5f746ffb0e9cdb00c76028ae553be0fecf
Yolan Uyttenhove <yuyttenhove@gmail.com>
2020-12-12 10:27:11 +0100

    testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::f64::consts::PI;
+use num::complex::Complex;
+use image::DynamicImage;
+
 fn main() {
-    println!("Hello, world!");
+    let x = Complex::new(0.0, 2.0*PI);
+
+    let n = x.norm_sqr();
+
+    let im = DynamicImage::new_rgb8(200, 150);
+    im.save("test.png");
+
+    println!("e^(2i * pi) = {}", x.exp()); // =~1
+    println!("norm squared is {}", n);
+    println!("norm is: {}", n.sqrt());
 }
