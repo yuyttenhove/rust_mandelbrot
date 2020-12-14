bbb519009e0e92248c0c1136a630c01e16a1ec05
Yolan Uyttenhove <yuyttenhove@gmail.com>
2020-12-14 20:42:11 +0100

    use modules

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,8 @@ use std::time::{Instant};
 fn main() {
     let start = Instant::now();
     let max_iter = 1024 as u16;
-    let im = construct_mandelbrot_image(Complex::new(-0.75, 0.), 1000, 1000, 5., 32, 32, max_iter);
+    let im = mandelbrot::construct_mandelbrot_image(Complex::new(-0.75, 0.), 1000, 1000, 5., 32, 32, max_iter);
     let duration = start.elapsed();
     println!("Image construction took: {:?}", duration);
     im.save("test.png").unwrap();
-
 }
