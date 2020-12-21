d7b8d4e029258317a9852c7d5191ef4e1477daca
Yolan Uyttenhove <yuyttenhove@gmail.com>
2020-12-21 14:24:28 +0100

    interactions work

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,66 @@
 mod mandelbrot;
 
 use num::complex::Complex;
-use std::time::{Instant};
-#[macro_use]
-extern crate glium;
-
-use std::io::Cursor;
-#[allow(unused_imports)]
 use glium::{glutin, Surface};
 
-fn main() {
-    let start = Instant::now();
+fn get_mandelbrot_opengl_texture(display: &glium::Display, center: Complex<f64>, image_dimensions: (u32, u32), width_x: f64)
+    -> glium::Texture2d {
     let max_iter = 1024 as u16;
+    let chunk_w = 32;
+    let chunk_h = 32;
+    let im = mandelbrot::construct_mandelbrot_image(center, image_dimensions.0 as usize, image_dimensions.1 as usize, width_x, chunk_w, chunk_h, max_iter);
+    glium::Texture2d::new(display,
+                          glium::texture::RawImage2d::from_raw_rgb(im.into_raw(),
+                                                                   image_dimensions))
+        .unwrap()
+}
+
+fn print_help_message(){
+    println!("Press I to zoom in, O to move out and use the arrow keys to move around.");
+    println!("Press S to save a high resolution image of the current view.");
+}
+
+fn save_high_res_mandelbrot_image(center: Complex<f64>, width_x: f64) {
+    let max_iter = 4096 as u16;
+    let chunk_w = 32;
+    let chunk_h = 32;
+    println!("Generating high res image");
+    let im = mandelbrot::construct_mandelbrot_image(center, 5760, 3240, width_x, chunk_w, chunk_h, max_iter);
+    im.save(format!("mandelbrot_({:.3e}, {:.3e})_{:.3e}.png", center.re, center.im, width_x)).unwrap();
+    println!("High res image saved!");
+}
+
+fn main() {
+    print_help_message();
     let mut width_x = 5.;
     let mut center = Complex::new(-0.75, 0.);
-    let mut im = mandelbrot::construct_mandelbrot_image(center, 1600, 1000, width_x, 32, 32, max_iter);
-    let duration = start.elapsed();
-    println!("Image construction took: {:?}", duration);
-    im.save("test.png").unwrap();
-    let image_dimensions = im.dimensions();
+    let movement_scale = 0.01;
+    let image_dimensions = (1600, 1000);
 
     let event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new().with_inner_size(glutin::dpi::PhysicalSize{width: image_dimensions.0, height: image_dimensions.1});
+    let wb = glutin::window::WindowBuilder::new()
+        .with_title("Mandelbrot explorer")
+        .with_inner_size(glutin::dpi::PhysicalSize{width: image_dimensions.0, height: image_dimensions.1});
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let image = glium::texture::RawImage2d::from_raw_rgb(im.into_raw(), image_dimensions);
-    let opengl_texture = glium::Texture2d::new(&display, image).unwrap();
+    let mut opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
 
     let dest_texture = glium::Texture2d::empty_with_format(&display,
                                                            glium::texture::UncompressedFloatFormat::U8U8U8U8,
                                                            glium::texture::MipmapsOption::NoMipmap,
                                                            image_dimensions.0, image_dimensions.1).unwrap();
     dest_texture.as_surface().clear_color(0.0, 0.0, 0.0, 1.0);
+    let (left, bottom, dimensions): (f32, f32, f32) = (0., 1., 1.);
+    let dest_rect = glium::BlitTarget {
+        left: (left * dest_texture.get_width() as f32) as u32,
+        bottom: (bottom * dest_texture.get_height().unwrap() as f32) as u32,
+        width: (dimensions * dest_texture.get_width() as f32) as i32,
+        height: (-1. * dimensions * dest_texture.get_height().unwrap() as f32) as i32,
+    };
 
-
+    let mut saved_location = false;
     event_loop.run(move |event, _, control_flow| {
-        let (left, bottom, dimensions): (f32, f32, f32) = (0., 0., 1.);
-        let dest_rect = glium::BlitTarget {
-            left: (left * dest_texture.get_width() as f32) as u32,
-            bottom: (bottom * dest_texture.get_height().unwrap() as f32) as u32,
-            width: (dimensions * dest_texture.get_width() as f32) as i32,
-            height: (dimensions * dest_texture.get_height().unwrap() as f32) as i32,
-        };
-
         opengl_texture.as_surface().blit_whole_color_to(&dest_texture.as_surface(), &dest_rect,
                                                         glium::uniforms::MagnifySamplerFilter::Linear);
 
@@ -52,7 +69,7 @@ fn main() {
         target.finish().unwrap();
 
         let next_frame_time = std::time::Instant::now() +
-            std::time::Duration::from_nanos(16_666_667);
+            std::time::Duration::from_nanos(33_333_333);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
         match event {
@@ -62,12 +79,49 @@ fn main() {
                     return;
                 },
                 glutin::event::WindowEvent::KeyboardInput {input, ..} => {
-                    //println!("Keyboardevent! {:?}", input);
-                    match input.virtual_keycode.unwrap() {
-                        glutin::event::VirtualKeyCode::S => {
-                            println!("S pressed!")
-                        },
-                        _ => {}
+                    if input.state == glutin::event::ElementState::Pressed {
+                        match input.virtual_keycode.unwrap() {
+                            glutin::event::VirtualKeyCode::Up => {
+                                center -= Complex::new(0., movement_scale * width_x);
+                                opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
+                                saved_location = false;
+                            },
+                            glutin::event::VirtualKeyCode::Down => {
+                                center += Complex::new(0., movement_scale * width_x);
+                                opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
+                                saved_location = false;
+                            },
+                            glutin::event::VirtualKeyCode::Left => {
+                                center -= Complex::new(movement_scale * width_x, 0.);
+                                opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
+                                saved_location = false;
+                            },
+                            glutin::event::VirtualKeyCode::Right => {
+                                center += Complex::new(movement_scale * width_x, 0.);
+                                opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
+                                saved_location = false;
+                            },
+                            glutin::event::VirtualKeyCode::I => {
+                                width_x *= 0.95;
+                                opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
+                                saved_location = false;
+
+                            },
+                            glutin::event::VirtualKeyCode::O => {
+                                width_x /= 0.95;
+                                opengl_texture = get_mandelbrot_opengl_texture(&display, center, image_dimensions, width_x);
+                                saved_location = false;
+                            },
+                            glutin::event::VirtualKeyCode::S => {
+                                if !saved_location {
+                                    saved_location = true;
+                                    save_high_res_mandelbrot_image(center, width_x);
+                                }
+                            },
+                            _ => {
+                                print_help_message();
+                            }
+                        }
                     }
                 },
                 _ => return,
